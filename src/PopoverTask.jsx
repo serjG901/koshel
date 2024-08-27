eb1e6d11b44cad90a830466bd389857289462cfc
serjG901 <70537823+serjG901@users.noreply.github.com>
2024-08-27 17:21:36 +0300

    Update PopoverTask.jsx

--- src/PopoverTask.jsx
+++ src/PopoverTask.jsx
@@ -16,75 +16,68 @@ export default function PopoverTask({ task }) {
   };
 
   return (
-<div key={task.id} className='task'>
-                    <div
-                      popover='auto'
-                      id={`update-popover-${task.id}`}
-                      className='popover'
-                    >
-                      <form
-                        action='submit'
-                        id='update-task'
-                        onSubmit={(e) => handleSubmitTaskUpdate(e, task.id)}
-                      >
-                        <div>
-                          <label htmlFor='task-date'>
-                            <span>Дата</span>
-                            <input
-                              type='date'
-                              id='task-date'
-                              required
-                              defaultValue={task.date}
-                            />
-                          </label>
-                          <label htmlFor='task-cost'>
-                            <span>Сумма</span>
-                            <input
-                              type='number'
-                              id='task-cost'
-                              required
-                              step='0.01'
-                              min='0.01'
-                              defaultValue={task.cost}
-                            />
-                          </label>
-                          <label htmlFor='task-description'>
-                            <span>Предмет/Услуга</span>
-                            <input
-                              type='text'
-                              id='task-description'
-                              required
-                              defaultValue={task.description}
-                            />
-                          </label>
-                          <label htmlFor='task-confirm'>
-                            Подтверждено
-                            <input
-                              type='checkbox'
-                              id='task-confirm'
-                              defaultChecked={task.confirm}
-                            />
-                          </label>
-                        </div>
-                        <button type='submit'>Обновить оплату</button>
-                      </form>
-                    </div>
+    <div key={task.id} className='task'>
+      <div popover='auto' id={`update-popover-${task.id}`} className='popover'>
+        <form
+          action='submit'
+          id='update-task'
+          onSubmit={(e) => handleSubmitTaskUpdate(e, task.id)}
+        >
+          <div>
+            <label htmlFor='task-date'>
+              <span>Дата</span>
+              <input
+                type='date'
+                id='task-date'
+                required
+                defaultValue={task.date}
+              />
+            </label>
+            <label htmlFor='task-cost'>
+              <span>Сумма</span>
+              <input
+                type='number'
+                id='task-cost'
+                required
+                step='0.01'
+                min='0.01'
+                defaultValue={task.cost}
+              />
+            </label>
+            <label htmlFor='task-description'>
+              <span>Предмет/Услуга</span>
+              <input
+                type='text'
+                id='task-description'
+                required
+                defaultValue={task.description}
+              />
+            </label>
+            <label htmlFor='task-confirm'>
+              Подтверждено
+              <input
+                type='checkbox'
+                id='task-confirm'
+                defaultChecked={task.confirm}
+              />
+            </label>
+          </div>
+          <button type='submit'>Обновить оплату</button>
+        </form>
+      </div>
 
-                    <div className='date'>{task.date}</div>
-                    <div className='cost'>{task.cost}</div>
-                    <div className='description'>{task.description}</div>
-                    <div
-                      className='confirm'
-                      data-confirm={task.confirm ? "Confirm" : "Not confirm"}
-                    >
-                      {task.confirm ? "Подтверждена" : "Не подтверждена"}
-                    </div>
-                    <button
-                      className='update'
-                      popovertarget={`update-popover-${task.id}`}
-                    >
-                      Обновить
-                    </button>
-                  </div>
+      <div className='date'>{task.date}</div>
+      <div className='cost'>{task.cost}</div>
+      <div className='description'>{task.description}</div>
+      <div
+        className='confirm'
+        data-confirm={task.confirm ? "Confirm" : "Not confirm"}
+      >
+        {task.confirm ? "Подтверждена" : "Не подтверждена"}
+      </div>
+      <button className='update' popovertarget={`update-popover-${task.id}`}>
+        Обновить
+      </button>
+    </div>
   );
 }
