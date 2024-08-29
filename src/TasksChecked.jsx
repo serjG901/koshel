9839e862af5f0cfdfff0d8e3eb9edd4023be8ee3
serjG901 <70537823+serjG901@users.noreply.github.com>
2024-08-29 09:47:20 +0300

    Update TasksChecked.jsx

--- src/TasksChecked.jsx
+++ src/TasksChecked.jsx
@@ -3,6 +3,7 @@ import StatTasks from "./StatTasks";
 import PopoverTask from "./PopoverTask";
 
 export default function TasksChecked({ tasksChecked }) {
+  let taskDate = 0;
   return (
     <details>
       <summary>Затраты</summary>
@@ -16,7 +17,20 @@ export default function TasksChecked({ tasksChecked }) {
                 Date.parse(new Date(task1.date))
             )
             .map((task) => {
-              return <PopoverTask key={task.id} task={task} />;
+              let first = false;
+              if (!taskDate) (taskDate = task.date), (first = true);
+              let equal = task.date === taskDate;
+              return equal ? (
+                <>
+                  {first && <div className='break-line'>{task.date}</div>}
+                  <PopoverTask key={task.id} task={task} />
+                </>
+              ) : (
+                <>
+                  <div className='break-line'>{task.date}</div>
+                  <PopoverTask key={task.id} task={task} />
+                </>
+              );
             })}
       </div>
     </details>
